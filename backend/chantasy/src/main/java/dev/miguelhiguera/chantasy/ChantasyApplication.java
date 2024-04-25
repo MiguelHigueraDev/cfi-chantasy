1d91a97904f9dc74400dadac52488e17c8a70832
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-04-25 15:13:07 -0400

    feat: add spring boot starter

---
+++ backend/chantasy/src/main/java/dev/miguelhiguera/chantasy/ChantasyApplication.java
@@ -0,0 +1,13 @@
+package dev.miguelhiguera.chantasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChantasyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChantasyApplication.class, args);
+    }
+
+}
