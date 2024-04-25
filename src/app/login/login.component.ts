f7280c8470f94fb90c284293c0cd865c99832a78
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-04-24 21:25:37 -0400

    feat(login): add register link to login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'ch-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink],
   templateUrl: './login.component.html',
   styles: ``
 })
