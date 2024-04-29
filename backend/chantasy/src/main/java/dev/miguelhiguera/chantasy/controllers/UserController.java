bee9f2a725011d10824943b1a5e89bca0d431558
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-04-29 18:07:52 -0400

    feat: add UserController
    returns current authenticated user and all users

---
+++ backend/chantasy/src/main/java/dev/miguelhiguera/chantasy/controllers/UserController.java
@@ -0,0 +1,39 @@
+package dev.miguelhiguera.chantasy.controllers;
+
+import dev.miguelhiguera.chantasy.models.User;
+import dev.miguelhiguera.chantasy.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequestMapping("/api/users")
+@RestController
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<User> authenticatedUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        User currentUser = (User) authentication.getPrincipal();
+
+        return ResponseEntity.ok(currentUser);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<User>> allUsers() {
+        return ResponseEntity.ok(userService.allUsers());
+    }
+}
