154d616008a6731bfbdf9f813ecd1ba6a596f6cf
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-04-29 18:08:16 -0400

    feat: add LoginResponse
    represents a login response returned from the server after authenticating. it contains the jwt

---
+++ backend/chantasy/src/main/java/dev/miguelhiguera/chantasy/responses/LoginResponse.java
@@ -0,0 +1,16 @@
+package dev.miguelhiguera.chantasy.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class LoginResponse {
+
+    private String token;
+    private long expiresIn;
+
+
+}
