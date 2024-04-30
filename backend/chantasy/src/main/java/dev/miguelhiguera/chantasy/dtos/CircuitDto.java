c44655264c72038688d6b1dde72697e40073407a
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-04-30 17:26:18 -0400

    fix(CircuitDto): change NotBlank to @NotNull

--- backend/chantasy/src/main/java/dev/miguelhiguera/chantasy/dtos/CircuitDto.java
+++ backend/chantasy/src/main/java/dev/miguelhiguera/chantasy/dtos/CircuitDto.java
@@ -1,6 +1,7 @@
 package dev.miguelhiguera.chantasy.dtos;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -11,6 +12,6 @@ public class CircuitDto {
     @Size(min = 3, max = 70, message = "El nombre del circuito debe tener 3-70 caracteres.")
     private String name;
 
-    @NotBlank(message = "El país del circuito no puede estar vacío.")
+    @NotNull(message = "El país del circuito no puede estar vacío.")
     private Long countryId;
 }
