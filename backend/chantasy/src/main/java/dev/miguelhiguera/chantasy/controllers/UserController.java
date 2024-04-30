2710e6aedb717831a0aba8d3279941fda4db497a
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-04-29 20:34:07 -0400

    fix(UserController): add role-based authentication

--- backend/chantasy/src/main/java/dev/miguelhiguera/chantasy/controllers/UserController.java
+++ backend/chantasy/src/main/java/dev/miguelhiguera/chantasy/controllers/UserController.java
@@ -4,6 +4,7 @@ import dev.miguelhiguera.chantasy.entities.User;
 import dev.miguelhiguera.chantasy.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,6 +25,7 @@ public class UserController {
     }
 
     @GetMapping("/me")
+    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<User> authenticatedUser() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
@@ -33,6 +35,7 @@ public class UserController {
     }
 
     @GetMapping("/")
+    @PreAuthorize("hasAnyRole('ADMIN', 'SUPER_ADMIN')")
     public ResponseEntity<List<User>> allUsers() {
         return ResponseEntity.ok(userService.allUsers());
     }
