8bd5db182cfe43ddd6c6419ad1103b5e43777f3e
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-04-29 21:00:44 -0400

    feat(UserService): add createAdministrator

--- backend/chantasy/src/main/java/dev/miguelhiguera/chantasy/services/UserService.java
+++ backend/chantasy/src/main/java/dev/miguelhiguera/chantasy/services/UserService.java
@@ -1,9 +1,11 @@
 package dev.miguelhiguera.chantasy.services;
 
+import dev.miguelhiguera.chantasy.dtos.RegisterUserDto;
 import dev.miguelhiguera.chantasy.entities.User;
 
 import java.util.List;
 
 public interface UserService {
     List<User> allUsers();
+    User createAdministrator(RegisterUserDto input);
 }
--- backend/chantasy/src/main/java/dev/miguelhiguera/chantasy/services/UserServiceImpl.java
+++ backend/chantasy/src/main/java/dev/miguelhiguera/chantasy/services/UserServiceImpl.java
@@ -1,26 +1,53 @@
 package dev.miguelhiguera.chantasy.services;
 
+import dev.miguelhiguera.chantasy.dtos.RegisterUserDto;
+import dev.miguelhiguera.chantasy.entities.Role;
+import dev.miguelhiguera.chantasy.entities.RoleEnum;
 import dev.miguelhiguera.chantasy.entities.User;
+import dev.miguelhiguera.chantasy.repositories.RoleRepository;
 import dev.miguelhiguera.chantasy.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService{
 
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public List<User> allUsers() {
         List<User> users = new ArrayList<>();
         userRepository.findAll().forEach(users::add);
         return users;
+    }
+
+    @Override
+    public User createAdministrator(RegisterUserDto input) {
+        Optional<Role> optionalRole = roleRepository.findByName(RoleEnum.ADMIN);
+
+        if (optionalRole.isEmpty()) {
+            throw new RuntimeException("Admin role not found.");
+        }
+
+        User user = new User();
+        user.setUsername(input.getUsername());
+        user.setDisplayName(input.getDisplayName());
+        user.setPassword(passwordEncoder.encode(input.getPassword()));
+        user.setRole(optionalRole.get());
+
+        return userRepository.save(user);
     }
 }
