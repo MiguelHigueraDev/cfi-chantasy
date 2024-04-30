184a63e5229d4e3ecdb237915666e154c6fa7dfb
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-04-30 15:30:55 -0400

    feat: add extra exceptions to GlobalExceptionHandler

--- backend/chantasy/src/main/java/dev/miguelhiguera/chantasy/exceptions/GlobalExceptionHandler.java
+++ backend/chantasy/src/main/java/dev/miguelhiguera/chantasy/exceptions/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package dev.miguelhiguera.chantasy.exceptions;
 
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.security.SignatureException;
+import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
 import org.springframework.security.authentication.AccountStatusException;
@@ -52,6 +54,21 @@ public class GlobalExceptionHandler {
         if (exception instanceof ExpiredJwtException) {
             errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(403), exception.getMessage());
             errorDetail.setProperty("description", "The JWT token has expired");
+        }
+
+        if (exception instanceof RuntimeException) {
+            errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(400), exception.getMessage());
+            errorDetail.setProperty("description", "Bad request");
+        }
+
+        if (exception instanceof EntityNotFoundException) {
+            errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(404), exception.getMessage());
+            errorDetail.setProperty("description", "Resource not found");
+        }
+
+        if (exception instanceof EntityExistsException) {
+            errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(409), exception.getMessage());
+            errorDetail.setProperty("description", "Resource already exists");
         }
 
         if (errorDetail == null) {
