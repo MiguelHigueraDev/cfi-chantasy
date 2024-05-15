c5d3c1ec79cc5f787119fa5125e4cffdff86952f
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-05-15 19:10:08 -0400

    feat(AnswerService): add date check and docs

--- backend/chantasy/src/main/java/dev/miguelhiguera/chantasy/services/impl/predictions/AnswerServiceImpl.java
+++ backend/chantasy/src/main/java/dev/miguelhiguera/chantasy/services/impl/predictions/AnswerServiceImpl.java
@@ -2,9 +2,11 @@ package dev.miguelhiguera.chantasy.services.impl.predictions;
 
 import dev.miguelhiguera.chantasy.dtos.predictions.AnswerDto;
 import dev.miguelhiguera.chantasy.dtos.predictions.AnswersListDto;
+import dev.miguelhiguera.chantasy.entities.Race;
 import dev.miguelhiguera.chantasy.entities.User;
 import dev.miguelhiguera.chantasy.entities.predictions.Answer;
 import dev.miguelhiguera.chantasy.entities.predictions.Question;
+import dev.miguelhiguera.chantasy.repositories.RaceRepository;
 import dev.miguelhiguera.chantasy.repositories.UserRepository;
 import dev.miguelhiguera.chantasy.repositories.predictions.AnswerRepository;
 import dev.miguelhiguera.chantasy.repositories.predictions.QuestionRepository;
@@ -13,6 +15,7 @@ import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -21,11 +24,13 @@ public class AnswerServiceImpl implements AnswerService {
     private final AnswerRepository answerRepository;
     private final UserRepository userRepository;
     private final QuestionRepository questionRepository;
+    private final RaceRepository raceRepository;
 
-    public AnswerServiceImpl(AnswerRepository answerRepository, UserRepository userRepository, QuestionRepository questionRepository) {
+    public AnswerServiceImpl(AnswerRepository answerRepository, UserRepository userRepository, QuestionRepository questionRepository, RaceRepository raceRepository) {
         this.answerRepository = answerRepository;
         this.userRepository = userRepository;
         this.questionRepository = questionRepository;
+        this.raceRepository = raceRepository;
     }
 
 
@@ -34,11 +39,26 @@ public class AnswerServiceImpl implements AnswerService {
         return answerRepository.findAllByQuestionId(questionId);
     }
 
+    /**
+     * Submit answers for the race. If the user has already submitted answers, they will be replaced.
+     * If the prediction period has ended, an exception will be thrown.
+     * @param input
+     * @param raceId
+     * @param userId
+     * @throws EntityNotFoundException
+     */
     @Transactional
     @Override
     public void submitAnswers(AnswersListDto input, Long raceId, Long userId) throws EntityNotFoundException {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new EntityNotFoundException("User not found"));
+
+        Race race = raceRepository.findById(raceId)
+                .orElseThrow(() -> new EntityNotFoundException("Race not found"));
+
+        if (LocalDateTime.now().isAfter(race.getPredictionEndDate())) {
+            throw new IllegalStateException("The prediction period for this race has ended.");
+        }
 
         List<Question> questions = questionRepository.findAllByRaceId(raceId);
 
