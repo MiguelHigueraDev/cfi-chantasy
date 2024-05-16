0c2616a22f73a1d553e020a2a21536b0f4c324c2
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-05-15 21:10:05 -0400

    fix: fix DateRangeValidator to take time into account as well

--- backend/chantasy/src/main/java/dev/miguelhiguera/chantasy/validators/DateRangeValidator.java
+++ backend/chantasy/src/main/java/dev/miguelhiguera/chantasy/validators/DateRangeValidator.java
@@ -5,22 +5,18 @@ import dev.miguelhiguera.chantasy.dtos.RaceDto;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
 public class DateRangeValidator implements ConstraintValidator<ValidDateRange, RaceDto> {
-    private static final DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE;
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
 
     @Override
     public boolean isValid(RaceDto raceDto, ConstraintValidatorContext context) {
-        if (raceDto == null) {
-            return true;
-        }
-
         try {
-            LocalDate startDate = LocalDate.parse(raceDto.getPredictionStartDate(), formatter);
-            LocalDate endDate = LocalDate.parse(raceDto.getPredictionEndDate(), formatter);
+            LocalDateTime startDate = LocalDateTime.parse(raceDto.getPredictionStartDate(), formatter);
+            LocalDateTime endDate = LocalDateTime.parse(raceDto.getPredictionEndDate(), formatter);
             return startDate.isBefore(endDate);
         } catch (DateTimeParseException e) {
             return false;
