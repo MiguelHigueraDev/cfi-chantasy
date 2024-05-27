fb4cfda35804aec931e890f24bc3854841c239b1
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-05-27 15:17:43 -0400

    feat(frontend): implement jwt renewal
    create dedicated clearSession and setSession methods
    add getExpiresIn and scheduleTokenRefresh methods
    add LoginResponse and Credentials interfaces
    use interfaces instead of any type
    
    update interceptor to support token refresh

--- website/src/app/interceptors/jwt.interceptor.ts
+++ website/src/app/interceptors/jwt.interceptor.ts
@@ -1,19 +1,42 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
 import { AuthService } from '../services/auth.service';
 import { inject } from '@angular/core';
+import { catchError, switchMap, throwError } from 'rxjs';
 
 export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService);
   const token = authService.getToken();
 
+  let clonedRequest = req;
+
   if (token) {
-    const clonedRequest = req.clone({
+    clonedRequest = req.clone({
       setHeaders: {
         Authorization: `Bearer ${token}`
       }
     });
-    return next(clonedRequest);
   }
+
   
-  return next(req);
+  return next(clonedRequest).pipe(
+    catchError((error: HttpErrorResponse) => {
+      if (error.status === 401) {
+        return authService.refreshToken().pipe(
+          switchMap(() => {
+            const newToken = authService.getToken();
+            if (newToken) {
+              clonedRequest = req.clone({
+                setHeaders: {
+                  Authorization: `Bearer ${newToken}`
+                }
+              });
+              return next(clonedRequest);
+            }
+            return throwError(() => error);
+          })
+        );
+      }
+      return throwError(() => error);
+    })
+  );
 };
--- website/src/app/services/auth.service.ts
+++ website/src/app/services/auth.service.ts
@@ -1,22 +1,23 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../environments/environment';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Router } from '@angular/router';
-import { map, Observable } from 'rxjs';
+import { catchError, map, Observable, tap, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private baseUrl = environment.apiUrl + '/auth';
+  private refreshTokenTimeout: any;
 
   constructor(private http: HttpClient, private router: Router) { }
 
-  login(credentials: any): Observable<any> {
-    return this.http.post<any>(`${this.baseUrl}/login`, credentials).pipe(
+  login(credentials: Credentials): Observable<LoginResponse> {
+    return this.http.post<LoginResponse>(`${this.baseUrl}/login`, credentials).pipe(
       map(response => {
-        if (response && response.token) {
-          localStorage.setItem('token', response.token);
+        if (response && response.token && response.refreshToken) {
+          this.setSession(response);
         }
         return response;
       })
@@ -28,7 +29,7 @@ export class AuthService {
   }
 
   logout(): void {
-    localStorage.removeItem('token');
+    this.clearSession();
     this.router.navigate(['/login']);
   }
 
@@ -36,8 +37,68 @@ export class AuthService {
     return localStorage.getItem('token');
   }
 
+  getRefreshToken(): string | null {
+    return localStorage.getItem('refreshToken');
+  
+  }
+
   isAuthenticated(): boolean {
     return !!this.getToken();
   }
 
+  refreshToken(): Observable<LoginResponse> {
+    const refreshToken = this.getRefreshToken();
+    if (!refreshToken) {
+      this.logout();
+      return throwError(() => new Error('No refresh token available'));
+    }
+
+    return this.http.post<LoginResponse>(`${this.baseUrl}/refresh-token`, { refreshToken }).pipe(
+      tap(response => {
+        if (response && response.token && response.refreshToken) {
+          this.setSession(response);
+        }
+      }),
+      catchError((error: HttpErrorResponse) => {
+        this.logout();
+        return throwError(() => error);
+      })
+    );
+  }
+
+  private setSession(response: LoginResponse): void {
+    localStorage.setItem('token', response.token);
+    localStorage.setItem('refreshToken', response.refreshToken);
+    localStorage.setItem('expiresIn', response.expiresIn / 1000 + '')
+    this.scheduleTokenRefresh();
+  }
+
+  private clearSession(): void {
+    localStorage.removeItem('token');
+    localStorage.removeItem('refreshToken');
+    clearTimeout(this.refreshTokenTimeout);
+  }
+
+  private getExpiresIn(): number {
+    return parseInt(localStorage.getItem('expiresIn') || '0', 10);
+  }
+
+  private scheduleTokenRefresh(): void {
+    const expiresIn = this.getExpiresIn();
+    this.refreshTokenTimeout = setTimeout(() => {
+      this.refreshToken().subscribe();
+    }, (expiresIn - 60) * 1000);
+  }
+
+}
+
+interface LoginResponse {
+  token: string;
+  expiresIn: number;
+  refreshToken: string;
+}
+
+interface Credentials {
+  username: string;
+  password: string;
 }
