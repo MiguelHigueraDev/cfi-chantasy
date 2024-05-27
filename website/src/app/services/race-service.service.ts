0065fcc5a0a8b1ca01e9c5957cd65806f9fddedd
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-05-26 20:16:00 -0400

    feat(frontend): add race service

---
+++ website/src/app/services/race-service.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Race } from '../interfaces/Race';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RaceService {
+
+  private baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  getNextRace(): Observable<Race> {
+    return this.http.get<Race>(`${this.baseUrl}/races/next`);
+  }
+
+}
