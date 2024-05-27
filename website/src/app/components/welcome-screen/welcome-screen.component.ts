01ff7c48222345adc667ead7b7bb12f2f0e3672e
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-05-26 20:16:09 -0400

    feat(frontend): add next race to home page

--- website/src/app/components/welcome-screen/welcome-screen.component.ts
+++ website/src/app/components/welcome-screen/welcome-screen.component.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { RaceService } from '../../services/race-service.service';
+import { Race } from '../../interfaces/Race';
+import { DateFormatPipe } from '../../pipes/date-format.pipe';
 
 @Component({
   selector: 'ch-welcome-screen',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, DateFormatPipe],
   templateUrl: './welcome-screen.component.html',
   styles: ``
 })
-export class WelcomeScreenComponent {
-  
+export class WelcomeScreenComponent implements OnInit {
+  nextRace: Race | null = null;
+
+  constructor(private raceService: RaceService) {}
+
+  ngOnInit(): void {
+    this.raceService.getNextRace().subscribe({
+      next: (data: Race) => {
+        this.nextRace = data;
+      },
+      error: (error) => {
+        console.error('Error obteniendo datos de la última carrera: ', error);
+      }
+    });
+  }
 
 }
