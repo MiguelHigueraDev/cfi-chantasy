74c2d8b1cd2c2ea4bdf0f4b72bad0303c5c8549c
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-05-28 16:20:24 -0400

    feat(frontend): add login and logout button

--- website/src/app/components/header/header.component.ts
+++ website/src/app/components/header/header.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'ch-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, MatButtonModule, MatIconModule, MatToolbarModule],
   templateUrl: './header.component.html',
   styles: ``
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  isAuthenticated: boolean = false;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authService.isAuthenticated$.subscribe(authenticated => {
+      this.isAuthenticated = authenticated;
+    });
+  }
+
+  logout(): void {
+    this.authService.logout();
+  }
 
 }
